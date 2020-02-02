be410580e6a5af7524ec5ac3e8a3462ff777d982
Donni Maulana Sipa <rabbit@Donnis-MacBook-Pro.local>
2020-02-02 09:28:54 +0700

    Updates

---
+++ precache-manifest.04b8f3bdca3ad9be1a9f05b2b5eaef4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9f98bb18c336d1482d2b8838fe9065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2169b47d148189bfbf91",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aac9996213e335637572",
+    "url": "/static/js/2.a42263c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a42263c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2169b47d148189bfbf91",
+    "url": "/static/js/main.d9903d5f.chunk.js"
+  },
+  {
+    "revision": "a0c3b831dbddfa3c7ab6",
+    "url": "/static/js/runtime-main.36ffe53f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
